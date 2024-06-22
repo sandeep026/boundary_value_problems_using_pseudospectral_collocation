ccc27aee31d1b7da4ca0f91f76ae825f7b203cca
Sandeepkumar R <rskravi15@gmail.com>
2024-06-22 17:43:15 +0530

    Update convergence.m

--- convergence.m
+++ convergence.m
@@ -1,12 +1,15 @@
 clear
 
-N=5:1:20;
+N=5:5:40;
 E=zeros(1,length(N));
 
 for i=1:length(N)
 E(i)=BVP(N(i));
 end
 
-semilogy(N,E)
-xlabel('grid size [N]')
-ylabel('discretization error')
+semilogy(N,E,'linewidth',3)
+xlabel('grid size [N]','FontSize',16)
+ylabel('discretization error','FontSize',16)
+set(gca,'FontSize',16)
+grid on
+print('exp.svg', '-dsvg', '-r600')
